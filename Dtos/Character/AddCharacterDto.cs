e8fe976ff1eacf08c548d7ea7c7bbf2acd02d51a
Alessandra Marusi <alessandra.marusi@icubed.it>
2024-02-05 16:48:48 +0100

    created Dtos

--- Dtos/Character/AddCharacterDto.cs
+++ Dtos/Character/AddCharacterDto.cs
@@ -2,6 +2,11 @@
 {
     public class AddCharacterDto
     {
-
+        public string Name { get; set; } = "Karlach";
+        public int HitPoints { get; set; } = 100;
+        public int Strenght { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Barbarian;
     }
 }
