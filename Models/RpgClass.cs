2766d4f687146cc6c70c27f56a74e77baa262ba8
Alessandra Marusi <alessandra.marusi@icubed.it>
2024-02-05 16:09:32 +0100

    initial commit

---
+++ Models/RpgClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))] //To show "Barbarian" instead of "0" in Swagger
+    public enum RpgClass
+    {
+        Barbarian = 0,
+        Fighter = 1,
+        Mage = 2,
+        Cleric = 3,
+    }
+}
